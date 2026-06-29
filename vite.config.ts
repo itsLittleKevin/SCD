c71415a366a971f87d9427378033b9bfef3996e2
KevinZ <me@itslittlekevin.com>
2026-06-29 21:02:31 +0800

    feat: add demo github link and production build entries

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        demo: fileURLToPath(new URL("./demo/index.html", import.meta.url)),
+      },
+    },
+  },
   server: {
+    allowedHosts: ["scd.i12n.io"],
     port: 5173,
     strictPort: true,
     proxy: {
